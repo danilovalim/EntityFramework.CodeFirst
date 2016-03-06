a65162cc65ae4bc6ba11f2d7c872a5c5e77e1792
Danilo Valim <danilovalim@hotmail.com>
2016-03-06 16:54:37 -0300

    First commit

---
+++ EF.CodeFirst/EF.CodeFirst/Controllers/ProductController.cs
@@ -0,0 +1,105 @@
+﻿using EF.CodeFirst.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EF.CodeFirst.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            using (StoreContext db = new StoreContext())
+            {
+                var products = db.product.ToList();
+                return View(products);
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View(new Product());
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product p)
+        {
+            if (ModelState.IsValid)
+            {
+                using (StoreContext db = new StoreContext())
+                {
+                    db.product.Add(p);
+                    db.SaveChanges();
+                    return RedirectToAction("List");
+                }
+            }
+            else
+            {
+                return View(p);
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            using (StoreContext db = new StoreContext())
+            {
+                Product prod = db.product.Find(id);
+                return View(prod);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Product p)
+        {
+            using (StoreContext db = new StoreContext())
+            {
+                Product prod = db.product.Where(x => x.ProductID == p.ProductID).First();
+                prod.Name = p.Name;
+                prod.Description = p.Description;
+                prod.Price = p.Price;
+                db.SaveChanges();
+                return RedirectToAction("List");
+            }
+        }
+
+        public ActionResult Details(int id)
+        {
+            using (StoreContext db = new StoreContext())
+            {
+                Product prod = db.product.Find(id);
+                return View(prod);
+            }
+        }
+           
+
+
+        public ActionResult Delete(int id)
+        {
+            using (StoreContext db = new StoreContext())
+            {
+                Product prod = db.product.Find(id);
+                return View(prod);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Product p, int id)
+        {
+            using (StoreContext db = new StoreContext())
+            {
+                Product prod = db.product.Find(id);
+                db.product.Remove(prod);
+                db.SaveChanges();
+                return RedirectToAction("List");
+            }
+        }
+    }
+}
---
+++ EF.CodeFirst/EF.CodeFirst/Models/StoreContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EF.CodeFirst.Models
+{
+    public class StoreContext : DbContext
+    {
+        public DbSet<Product> product { get; set; }
+    }
+}
